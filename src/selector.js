caefaec7f64852b33c4c866c6b938b9cd7418833
djgallag <djgallag@umich.edu>
2020-07-16 20:47:50 -0400

    added the base of the class

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,19 @@
 // YOUR CODE HERE
+
+
+class $ {
+    constructor(str) {
+        const className = [];
+        const id = [];
+        const tagName = [];
+        if (str[0] === '.') {
+            className.push(str.slice(1));
+        }
+        else if (str[0] === '#') {
+            id.push(str.slice(1));
+        }
+        else {
+            tagName.push(str.toUpperCase());
+        }
+    }
+}
